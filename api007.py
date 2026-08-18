39a18fe01222cc48c5e01fbc2f8f71a1c3d72827
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-18 17:59:12 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -101,7 +101,7 @@ headers4 = {
     }
 conn1.request(
     'GET',
-    '/openapi/typea/instruments/intraday/1/{para1}/5minute',
+    '/openapi/typea/instruments/intraday/1/26000/5minute',
     headers=headers4
 )
 response6 = conn1.getresponse()
