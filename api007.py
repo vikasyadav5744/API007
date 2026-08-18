21deac34e2d2205ba2ee9132bca260cb6b24b266
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-18 11:20:39 +0530

    Change API request method in api007.py
    Refactor API request handling to use http.client instead of requests.

--- api007.py
+++ api007.py
@@ -61,60 +61,26 @@ access_token = st.sidebar.text_input("Acess token", key='key7')
 #instrument_token = st.sidebar.text_input("instrument_token", value = 26000, key='key10')
 #interval = st.sidebar.selectbox("Interval", options = ['minute', '3minute', '5minute', '10minute', '15minute', '30minute', '60minute', 'day'], index = 2,key='key11')
 
-
 submit2 = st.sidebar.button("Get Data", key="key8")
 
-if submit2:
 
-    headers3 = {
-        "X-Mirae-Version": "1",
-        "Authorization": f"token {api_key}:{access_token}"
-    }
+conn = http.client.HTTPSConnection('api.mstock.trade')
+headers3 = {
+    'X-Mirae-Version': '1',
+    'Authorization': 'token api_Key:access_token',
+}
+conn.request(
+    'GET',
+    'openapi/typea/getoptionchainmaster/2',
+    headers=headers3
+)
+response5 = conn.getresponse()
 
-    url = "https://api.mstock.trade/openapi/typea/getoptionchainmaster/2"
-
-    response5 = requests.get(
-        url,
-        headers=headers3,
-        timeout=30
-    )
-
+if submit2==True:
     st.write("Status Code:", response5.status_code)
+    st.write(response5)
 
-    if response5.ok:
-
-        response6 = response5.json()
-
-        st.write(response6)
-
-        # If response is a list
-        if isinstance(response6, list):
-            df2 = pd.DataFrame(response6)
-            st.dataframe(df2, use_container_width=True)
-
-        # If response is a dictionary
-        elif isinstance(response6, dict):
-
-            st.json(response6)
-
-            # If actual data is inside "data"
-            if "data" in response6:
-
-                data = response6["data"]
-
-                if isinstance(data, list):
-                    df2 = pd.DataFrame(data)
-                    st.dataframe(df2, use_container_width=True)
-
-    else:
-        st.error(
-            f"API Error: {response5.status_code}"
-        )
-
-        st.code(response5.text)
-
-        
-
+   
 
 
 
