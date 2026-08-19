42be25fefa824c54508f0c97bda9682676634183
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-19 11:21:45 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -111,7 +111,8 @@ if submit3:
     response_text2 = response6.read().decode("utf-8")
     data2 = json.loads(response_text2)
     data3 = st.json(data2)
-    st.write(data3)
+    new= pd.Series(data3)
+    st.write(new)
    
 
 
