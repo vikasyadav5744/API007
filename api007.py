f100985fd14109ec037b6909944f3a54ee832c17
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-19 12:41:26 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="m.Stock option chain", layout="wide")
 
 st.title("Mirae Asset m.Stock option chain")
 
-api_key = str(“tl65K+S8+ZX4Q6i1kztrH20cVqafynuY3OLeAuT7Ay0=”)
+api_key = str(tl65K+S8+ZX4Q6i1kztrH20cVqafynuY3OLeAuT7Ay0=)
 
 # Sidebar
 st.sidebar.header("API credentials")
