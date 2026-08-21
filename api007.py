fbadc8e295a777fbdf1f53bfa14e60fee2baf920
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-21 10:46:52 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -198,6 +198,8 @@ response = conn.getresponse()
 
 if st.sidebar.button("Chain Master"):
     st.write('HTTPS status:', response.status)
+    st.json(response)
+    st.json(response.text)
     
 
 
