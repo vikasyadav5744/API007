b82fb0557a3e2856dced182b6aa5180ed5df39da
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-24 17:58:59 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -258,7 +258,7 @@ headers101 = {
 }
 conn.request(
     'GET',
-    'openapi/typea/GetOptionChain/2/1429972200/22',
+    'openapi/typea/GetOptionChain/2/11498746600/22',
     headers=headers101
 )
 response101 = conn.getresponse()
