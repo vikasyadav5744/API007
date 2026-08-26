403c5b972aee58c3c3977398d97378f9c91be1ee
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-26 22:33:19 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -229,7 +229,7 @@ if call_criteria:
 hist_criteria=st.sidebar.checkbox("Historical Data", key='hist_criteria')
 if hist_criteria:
   exchange_str= st.sidebar.selectbox("Exchane", key='exchange', options=['NSE','NFO','BSE','BFO'], index=0)
-  inst_token=st.sidebar.number_input("Instrument Token", key='instrument')
+  inst_token=int(st.sidebar.number_input("Instrument Token", key='instrument'))
   interval_hist= st.sidebar.selectbox("Choose Interval", key="interval_hist", options=['minute','5minute','10minute', '15minute', '30minute', '60minute', 'day'])
   if st.sidebar.button("Historical Data", key="historical_data"):
     conn.request(
