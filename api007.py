597dad1c9912232e7e97991b10382dbf4a451b35
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-26 22:30:55 +0530

    Change button for historical data retrieval

--- api007.py
+++ api007.py
@@ -231,7 +231,7 @@ if hist_criteria:
   exchange_str= st.sidebar.selectbox("Exchane", key='exchange', options=['NSE','NFO','BSE','BFO'], index=0)
   inst_token=st.sidebar.number_input("Instrument Token", key='instrument')
   interval_hist= st.sidebar.selectbox("Choose Interval", key="interval_hist", options=['minute','5minute','10minute', '15minute', '30minute', '60minute', 'day'])
-  if callmaster:
+  if st.sidebar.button("Historical Data", key="historical_data"):
     conn.request(
     'GET',
     f'/openapi/typea/instruments/historical/{exchange_str}/{inst_token}/{interval_hist}?from=2026-08-25&to=2026-08-26',
