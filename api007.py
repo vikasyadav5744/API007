a4bbe0f73e1d16dfb56f56ed63214eb8d912a5d4
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-28 10:46:40 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -302,13 +302,13 @@ if call_criteria:
     calldf = pd.DataFrame(call_rows, columns=['CE.token','CE.strike','CE.OI','CE.ChngOI']).fillna(0, inplace=True)
     calldf = calldf.astype('int64')
     calldf['strike'] =calldf['strike']/100
-    calldf_refined = calldf[calldf.strike.between(22000, 25000)]
+    calldf_refined = calldf[calldf['CE.strike'].between(22000, 25000)]
     st.write("calldf_refined:", calldf_refined)
     st.write("Dtypes:", calldf.dtypes)
     putdf = pd.DataFrame(put_rows, columns=['PE.token','PE.strike','PE.OI','PE.ChngOI']).fillna(0, inplace=True)
     putdf = putdf.astype('int64')
     putdf['strike'] = putdf['strike']/100
-    putdf_refined = putdf[putdf.strike.between(22000, 25000)]
+    putdf_refined = putdf[putdf['PE.strike'].between(22000, 25000)]
     option_chain =pd.concat([calldf_refined,putdf_refined], axis=1, ignore_index=False)
     st.dataframe(option_chain)
 
