247ea5d2729b6d731977daae75e76ee75d3b8955
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-28 15:52:32 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -333,8 +333,9 @@ st.write(list_epoch[0])
 expiry_detail = []
 response1 = requests.get(f"{url}/openapi/typea/GetOptionChain/2/{list_epoch[0]}/26000", headers=headers3)
 st.write("status", response1.status_code)
-response1 = response1.json()
-expiry = response1["data"]["contractModel"]["exp"]
+result101 = response1.read().decode("utf-8")
+data101= json.loads(result101)
+expiry = data101["data"]["contractModel"]["exp"]
 expiry_detail = expiry_detail.append(expiry)
 
 
