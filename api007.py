45d7a28f84a224c38b1e333c08ac87b0aac8ec73
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-28 10:13:25 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -300,7 +300,8 @@ if call_criteria:
     call_rows = parse_option_data(call_data)
     put_rows = parse_option_data(put_data)
     calldf = pd.DataFrame(call_rows, columns=['token','strike','CE.OI','CE.ChngOI']).fillna(0, inplace=True)
-    calldf['strike'] = calldf['strike'].div(100)
+    #calldf['strike'] = calldf['strike']
+    st.write(type(calldf['strike'])
     putdf = pd.DataFrame(put_rows, columns=['token','strike','PE.OI','PE.ChngOI']).fillna(0, inplace=True)
     st.dataframe(calldf)
     st.dataframe(putdf)
