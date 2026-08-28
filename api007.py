8628bc6cb3f6eb25964decf92adacaa8812cb81e
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-28 16:19:02 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -349,12 +349,12 @@ calldf_d = pd.DataFrame(call_rows_d, columns=['CE.token','CE.strike','CE.OI','CE
 calldf_d = calldf_d.astype('int64')
 calldf_d['CE.strike'] =calldf_d['CE.strike']/100
 calldf_refined_d = calldf_d[calldf_d['CE.strike'].between(strike1_d, strike2_d)]
-calldf_refined_d['CE.expiry'] = expiry
+calldf_refined_d['CE.expiry'] = expiry101
 putdf_d = pd.DataFrame(put_rows_d, columns=['PE.token','PE.strike','PE.OI','PE.ChngOI']).fillna(0, inplace=True)
 putdf_d = putdf_d.astype('int64')
 putdf_d['PE.strike'] = putdf_d['PE.strike']/100
 putdf_refined_d = putdf_d[putdf_d['PE.strike'].between(strike1_d, strike2_d)]
-putdf_refined_d['PE.expiry'] = expiry
+putdf_refined_d['PE.expiry'] = expiry101
 option_chain_d =pd.concat([calldf_refined_d,putdf_refined_d], axis=1, ignore_index=False)
 st.dataframe(option_chain_d, column_order=['CE.token','CE.OI','CE.ChngOI','CE.strike','PE.ChngOI','PE.OI','PE.token', 'CE.expiry'])
 
