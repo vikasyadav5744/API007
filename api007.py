068ff734e7c896339a06350acad5c1eef1e09bf2
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-08-30 13:31:37 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -312,7 +312,7 @@ call_criteria=st.sidebar.checkbox("Contract Master Data", key='call_criteria')
 if call_criteria:
   strike1=st.sidebar.number_input("select first strike", 21000, 28000, 23500, 50, key='strike1')
   strike2=st.sidebar.number_input("select second strike", 21000, 28000, 24500, 50, key='strike2')
-  expiry= st.sidebar.selectbox("Epoch", key='expiry', options=epoch_list, index=0)
+  expiry= st.sidebar.selectbox("Epoch", key='expiry', options=epoch_list, index=6)
   token1=st.sidebar.number_input("select token", value=26000, key='token1')
   callmaster=st.sidebar.button("Get Contract Master CE/PE", key='callmaster')
   if callmaster:
