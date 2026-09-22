3a352e08d4de260093a3f23089ea60f1da81979c
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-09-22 12:27:39 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import json
+from io import StringIO
 from datetime import datetime, timezone, date
 
 my_ip = requests.get("https://api.ipify.org", timeout=10).text
