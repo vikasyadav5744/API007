907b6427b1230d714367af9c2165cd5080491f6d
vikasyadav5744 <vikram.kumar500@gmail.com>
2026-09-25 12:35:52 +0530

    Update api007.py

--- api007.py
+++ api007.py
@@ -10,7 +10,7 @@ from datetime import datetime, timezone, date
 my_ip = requests.get("https://api.ipify.org", timeout=10).text
 st.write("Current public IP:", my_ip)
 
-data = st.file_uploader("csv file upload", key='upload1', type=None, accept_multiple_files=True)
+data = st.file_uploader("upload file upload", key='upload1', accept_multiple_files=True)
 if data is not None:
   data1 = pd.read_excel(data)
   st.write(data1)
